10dfa61ddb0c7d689fa1268f54da71baab199cf0
milei02 <45421134+milei02@users.noreply.github.com>
2018-12-04 09:14:03 +0100

    Add date for datepicker

--- src/SwissTransport/ITransport.cs
+++ src/SwissTransport/ITransport.cs
@@ -6,7 +6,7 @@ namespace SwissTransport
     {
         Stations GetStations(string query);
         StationBoardRoot GetStationBoard(string station, string id);
-        Connections GetConnections(string fromStation, string toStattion);
+        Connections GetConnections(string fromStation, DateTime dateTime, string toStattion);
 
         //Connections GetConnections(string fromStation, DateTime dateTime, string toStattion);
 
